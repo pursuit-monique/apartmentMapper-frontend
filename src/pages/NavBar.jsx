319dc506902a327a8d42d850646a310a64c34dab
Monique <107360758+pursuit-monique@users.noreply.github.com>
2024-06-11 13:12:37 -0400

    Cleaned up code, improved loading.  Removed Firebase

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -1,10 +1,10 @@
-import Controls from "./Controls";
+import Controls from "./Control";
 import './NavBar.css'; 
 
-export default function NavBar({setApartmentList, apartmentList, selectedValue, setSelectedValue, selection}) {return (
-// {/* <div className="navbar bg-base-100 bg-light bg-gradient bg-opacity-10 border border-bottom"> */}
-<div className="navbar navbar-expand-lg navbar-transparent border border-bottom">
-  
-  <Controls setApartmentList={setApartmentList} apartmentList={apartmentList} selectedValue={selectedValue} setSelectedValue={setSelectedValue} selection={selection} />
-</div>)
+export default function NavBar({setApartmentList, apartmentList, selectedValue, setSelectedValue, selection}) {
+  return (
+    <div className="navbar navbar-expand-lg navbar-transparent border border-bottom">
+      <Controls setApartmentList={setApartmentList} apartmentList={apartmentList} selectedValue={selectedValue} setSelectedValue={setSelectedValue} selection={selection} />
+    </div>
+    )
 }
